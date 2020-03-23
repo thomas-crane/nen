4ffc92494fae6463be4054a937a4c3fe6a1e4ec1
thomas-crane <thomasgcrane@gmail.com>
2020-03-23 22:45:27 +1100

    feat(main): add some test code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+mod node_version;
+mod path;
+mod version_list;
+
+use crate::node_version::NodeVersion;
+use crate::version_list::VersionList;
+use std::convert::TryFrom;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let arg = std::env::args()
+        .skip(1)
+        .next()
+        .expect("Provide a version to check.");
+    let version = NodeVersion::try_from(arg)?;
+    let version_list = VersionList::create().await?;
+    if version_list.has_version(&version.clone().into()) {
+        println!("{:?} is a valid Node.js version", version);
+    } else {
+        println!("{:?} is not a valid Node.js version", version);
+    }
+    Ok(())
 }
