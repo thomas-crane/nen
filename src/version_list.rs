31f66421d24aeb9af7eefd68e17f76d5fa836bac
thomas-crane <thomasgcrane@gmail.com>
2020-03-24 19:37:46 +1100

    feat(version_list): add latest_version_of method

--- src/version_list.rs
+++ src/version_list.rs
@@ -3,6 +3,8 @@ use reqwest;
 use scraper::{Html, Selector};
 use semver::Version;
 
+use crate::node_version::NodeVersion;
+
 const NODE_URL: &'static str = "https://nodejs.org/dist/";
 
 pub struct VersionList {
@@ -16,17 +18,27 @@ impl<'a> VersionList {
         let body = Html::parse_fragment(&body);
         let selector = Selector::parse("a").unwrap();
         let version_pattern = Regex::new(r"v(\d+\.\d+\.\d+)/")?;
-        let versions: Vec<Version> = body
+        let mut versions: Vec<Version> = body
             .select(&selector)
             .filter_map(|item| item.value().attr("href"))
             .filter(|version| version_pattern.is_match(version))
             .map(|version| &version[1..version.len() - 1])
             .filter_map(|version| Version::parse(version).ok())
             .collect();
+        // sort and reverse the list so that when we are
+        // translating something like "12.x.x" into the most
+        // recent version, we can just take the first
+        // version that has a major version of 12.
+        versions.sort_unstable();
+        versions.reverse();
         Ok(Self { versions })
     }
 
     pub fn has_version(&self, version: &Version) -> bool {
         self.versions.contains(version)
+    }
+
+    pub fn latest_version_of(&self, node_version: &NodeVersion) -> Option<&Version> {
+        self.versions.iter().find(|v| *v == node_version)
     }
 }
