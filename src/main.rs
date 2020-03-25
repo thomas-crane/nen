35ee1a68e9886a756012f65f76b9fd3e6023cf10
thomas-crane <thomasgcrane@gmail.com>
2020-03-25 22:59:35 +1100

    feat(main): add clap app

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,64 @@ mod version_list;
 use crate::nen_home::NenHome;
 use crate::node_version::NodeVersion;
 use std::convert::TryFrom;
+use clap::{App, Arg, AppSettings};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut arg_iter = std::env::args().skip(1);
-    let name = arg_iter.next().expect("Provide a name");
-    let version = arg_iter.next().expect("Provide a version");
+    // get the commands.
+    let matches = App::new("nen")
+        .setting(AppSettings::SubcommandRequired)
+        .subcommand(App::new("init").about("Initialise your nen home."))
+        .subcommand(
+            App::new("new")
+                .about("Create a new environment.")
+                .arg(
+                    Arg::with_name("name")
+                        .help("The name of the environment.")
+                        .index(1)
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("version")
+                        .help("The node version to use.")
+                        .index(2)
+                        .required(true),
+                ),
+        ).get_matches();
 
-    let version = NodeVersion::try_from(version)?;
-
+    // check for nen init
+    if matches.subcommand_name() == Some("init") {
+        let home = NenHome::new()?;
+        if home.is_valid_home() {
+            println!("Your home has already been initialised.");
+            return Ok(());
+        } else {
+            home.init_home()?;
+            println!("Successfully initialised nen home.");
+            return Ok(());
+        }
+    }
+    // create the home
     let home = NenHome::new()?;
-    let home = home.init_home()?;
+    if !home.is_valid_home() {
+        let path = home.home_path();
+        println!(
+            "Your current nen home ({}) is not initialised. Run `nen init` to do this.",
+            path.to_str().unwrap_or("unknown"),
+        );
+        return Ok(());
+    }
+    let home = home.validate()?;
 
-    home.create_env(&name, &version).await?;
-    Ok(())
+    match matches.subcommand_name() {
+        Some("new") => {
+            let matches = matches.subcommand_matches("new").unwrap();
+            let name = matches.value_of("name").unwrap();
+            let version = matches.value_of("version").unwrap();
+            let version = NodeVersion::try_from(version)?;
+            home.create_env(&String::from(name), &version).await?;
+            Ok(())
+        }
+        _ => unreachable!()
+    }
 }
