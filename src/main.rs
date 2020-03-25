37f64ef881177c0bde952e31453435edd6600056
thomas-crane <thomasgcrane@gmail.com>
2020-03-25 21:46:31 +1100

    chore(main): more test code

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,19 @@ mod version_list;
 
 use crate::nen_home::NenHome;
 use crate::node_version::NodeVersion;
-use crate::version_list::VersionList;
 use std::convert::TryFrom;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let arg = std::env::args()
-        .skip(1)
-        .next()
-        .expect("Provide a version to check.");
-    let version = NodeVersion::try_from(arg)?;
-    let version_list = VersionList::create().await?;
-    if version_list.has_version(&version.clone().into()) {
-        println!("{} is a valid Node.js version", version);
-        let home = NenHome::new()?;
-        let home = home.init_home()?;
-        println!(
-            "Downloading Node.js v{}",
-            version_list.latest_version_of(&version).unwrap()
-        );
-        home.download_node_version(&version, &version_list).await?;
-        println!("Done");
-    } else {
-        println!("{} is not a valid Node.js version", version);
-    }
+    let mut arg_iter = std::env::args().skip(1);
+    let name = arg_iter.next().expect("Provide a name");
+    let version = arg_iter.next().expect("Provide a version");
+
+    let version = NodeVersion::try_from(version)?;
+
+    let home = NenHome::new()?;
+    let home = home.init_home()?;
+
+    home.create_env(&name, &version).await?;
     Ok(())
 }
