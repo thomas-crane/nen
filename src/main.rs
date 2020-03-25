c8ec954dcece6816dcea88bba22f419c849b9985
thomas-crane <thomasgcrane@gmail.com>
2020-03-25 19:04:04 +1100

    chore(main): more test code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-mod node_version;
 mod nen_home;
+mod node_downloader;
+mod node_version;
+mod platform_string;
 mod version_list;
 
+use crate::nen_home::NenHome;
 use crate::node_version::NodeVersion;
 use crate::version_list::VersionList;
 use std::convert::TryFrom;
@@ -15,13 +18,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let version = NodeVersion::try_from(arg)?;
     let version_list = VersionList::create().await?;
     if version_list.has_version(&version.clone().into()) {
-        println!("{:?} is a valid Node.js version", version);
+        println!("{} is a valid Node.js version", version);
+        let home = NenHome::new()?;
+        let home = home.init_home()?;
+        println!(
+            "Downloading Node.js v{}",
+            version_list.latest_version_of(&version).unwrap()
+        );
+        home.download_node_version(&version, &version_list).await?;
+        println!("Done");
     } else {
-        println!("{:?} is not a valid Node.js version", version);
-    }
-    let latest = version_list.latest_version_of(&version);
-    if let Some(v) = latest {
-        println!("The latest version of {:?} is '{}'", &version, v);
+        println!("{} is not a valid Node.js version", version);
     }
     Ok(())
 }
